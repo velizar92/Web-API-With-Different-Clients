7968fffceb66246ec65947b82dd16157b08f8ef9
zako <velizar9209@gmail.com>
2022-07-30 21:46:06 +0300

    UI updates for web console client

--- WebAPIWithDifferentClients/ProgrammingConsoleWebClient/Program.cs
+++ WebAPIWithDifferentClients/ProgrammingConsoleWebClient/Program.cs
@@ -5,17 +5,55 @@
     using System.Net.Http;
     using System.Text.Json;
     using System.Threading.Tasks;
+    using ConsoleTables;
     using OfficeOpenXml;
     using ProgrammingConsoleWebClient.ViewModels;
 
     internal class Program
-    {
-        private static readonly HttpClient _client = new HttpClient();
+    {     
         static async Task Main(string[] args)
         {
+            int id = 0;
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-            ConsoleLogger consoleLogger = new ConsoleLogger();
-            HttpWebClient webClient = new HttpWebClient(consoleLogger);
+            ILogger consoleLogger = new ConsoleLogger();
+            IHttpWebClient webClient = new HttpWebClient(consoleLogger);
+
+            ShowMenu();
+
+            Console.WriteLine("Please choose your option: ");
+            int optionNumber = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                switch (optionNumber)
+                {
+                    case 1:
+                        
+                        break;
+                    case 2:
+                        var languages = await webClient.GetResources("https://localhost:7046/api/ProgrammingLanguages/all");                      
+                        PrintLanguages(languages);
+                        break;
+                    case 3:
+                        Console.WriteLine("Please type resource id: ");
+                        id = int.Parse(Console.ReadLine());
+                        var language = await webClient.GetResource("https://localhost:7046/api/ProgrammingLanguages/", id);
+                        PrintLanguage(language);
+                        break;
+                    case 4:                      
+                        break;
+                    case 5:  
+                        
+                        break;
+                    default:
+                        Console.WriteLine("Unknown option");
+                        break;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Please choose your next option: ");
+                optionNumber = int.Parse(Console.ReadLine());
+            }
 
             //ProgrammingLanguage programmingLanguage = new ProgrammingLanguage()
             //{
@@ -24,26 +62,58 @@
             //    Tutorials = new List<Tutorial>()
             //};
 
-            //await CreateResource("https://localhost:7046/api/ProgrammingLanguages/create", programmingLanguage);
+            //await webClient.CreateResource("https://localhost:7046/api/ProgrammingLanguages/create", programmingLanguage);
 
-            //await webClient.DeleteResource("https://localhost:7046/api/ProgrammingLanguages/delete/19");
+            //await webClient.DeleteResource("https://localhost:7046/api/ProgrammingLanguages/delete/2");
 
-            IEnumerable<ProgrammingLanguage> languages
-                = await webClient.GetResource("https://localhost:7046/api/ProgrammingLanguages/all");
+            //ProgrammingLanguage updatedPogrammingLanguage = new ProgrammingLanguage()
+            //{
+            //    Id = 5,
+            //    Name = "Python",
+            //    Description = "Python is cool language and will be updated soon.",
+            //    Tutorials = new List<Tutorial>()
+            //};
 
-            foreach (var language in languages)
+            //await webClient.UpdateResource("https://localhost:7046/api/ProgrammingLanguages/update/5", updatedPogrammingLanguage);
+
+        }
+
+        static void ShowMenu()
+        {
+            var table = new ConsoleTable("Coomand Index", "Command", "Command Description");
+
+            table.AddRow("1.", "Create", "Create a new resource")
+                 .AddRow("2.", "GetAll", "Get all resources on the specified URL.")
+                 .AddRow("3.", "GetById","Get resource specified by id.")
+                 .AddRow("4.", "Update", "Update resource specified by id.")
+                 .AddRow("4.", "Delete", "Delete resource specified by id.")
+                 .AddRow("5.", "Export in Excel", "Export the previous command result in Excel file.");
+
+            table.Write();
+            Console.WriteLine();
+        }
+
+
+        static void PrintLanguages(IEnumerable<ProgrammingLanguage> programmingLanguages)
+        {
+            var table = new ConsoleTable("Id", "Name", "Description");
+        
+            foreach (var programmingLanguage in programmingLanguages)
             {
-                Console.WriteLine(language.Id);
-                Console.WriteLine(language.Name);
-                Console.WriteLine(language.Description);
-                Console.WriteLine("======Tutorials:======");
-
-                foreach (var tutorial in language.Tutorials)
-                {
-                    Console.WriteLine(tutorial.Name);
-                    Console.WriteLine(tutorial.Description);
-                }
+                table.AddRow(programmingLanguage.Id, programmingLanguage.Name, programmingLanguage.Description);            
             }
+
+            table.Write();
+        }
+
+        static void PrintLanguage(ProgrammingLanguage programmingLanguage)
+        {
+            if (programmingLanguage != null)
+            {
+                var table = new ConsoleTable("Id", "Name", "Description");
+                table.AddRow(programmingLanguage.Id, programmingLanguage.Name, programmingLanguage.Description);
+                table.Write();
+            }    
         }
 
     }
